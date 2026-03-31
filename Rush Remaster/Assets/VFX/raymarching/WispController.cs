edf40fd61d804b80e230de07720a2b4500a9ecba
St0venator <104474628+St0venator@users.noreply.github.com>
2026-03-31 16:05:18 -0400

    Tutorial and the Boss

--- Rush Remaster/Assets/VFX/raymarching/WispController.cs
+++ Rush Remaster/Assets/VFX/raymarching/WispController.cs
@@ -6,31 +6,23 @@ using UnityEngine;
 public class WispController : MonoBehaviour
 {
     public float size;
+    float currSize;
     public string[] dialogue;
     public GameObject dialogueObj;
     public bool started;
+    public MeshRenderer MR;
+    Material Mat;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mat = MR.material;
+        currSize = size;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                started = true;
-                dialogueObj.SetActive(true);
-                dialogueObj.GetComponent<TextMeshProUGUI>().text = dialogue[0];
-                if (dialogue.Length > 1)
-                {
-                    StartCoroutine(displayDialogue());
-                }
-            }
-        }
+        
         
     }
 
@@ -42,7 +34,45 @@ public class WispController : MonoBehaviour
             yield return new WaitForSeconds(5);
             i++;
             dialogueObj.GetComponent<TextMeshProUGUI>().text = dialogue[i];
+
+            if (dialogue[i] == "Penis")
+            {
+                dialogueObj.SetActive(false);
+                this.StopAllCoroutines();
+                StartCoroutine (die());
+            }
         }
         
+    }
+
+    IEnumerator die()
+    {
+        while(currSize / size >= 0)
+        {
+            Debug.Log("dying");
+            yield return new WaitForSeconds(Time.deltaTime);
+
+            currSize -= Time.deltaTime;
+
+            Mat.SetVector("_sphere", new Vector4(0, 0, 0, Mathf.Lerp(0f, size, currSize / size)));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (!started)
+            {
+
+                    started = true;
+                    dialogueObj.SetActive(true);
+                    dialogueObj.GetComponent<TextMeshProUGUI>().text = dialogue[0];
+                    if (dialogue.Length > 1)
+                    {
+                        StartCoroutine(displayDialogue());
+                    }
+            }
+        }
     }
 }
